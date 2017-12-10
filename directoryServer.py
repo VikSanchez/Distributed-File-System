41ebdabc60a4e73727afe208a151363f46fd7f7e
VikasDanny <vikasdanny56@gmail.com>
2017-12-10 21:56:10 +0000

    Update directoryServer.py

--- directoryServer.py
+++ directoryServer.py
@@ -16,27 +16,39 @@ class client_thread(Thread):
                      get_list(self.addr,self.c)
               
 def get_list(name,sock):
-    f_name = (sock.recv(2048)).decode()
-    info = (sock.recv(2048)).decode()
-    f = open(f_name,"w+")
-    f.write(info)
-    print('done')
-    f.close()
+
+    inp = (sock.recv(2048)).decode()
+    print inp
     
+    if inp == '1':
+           f_name = (sock.recv(2048)).decode()
+           info = (sock.recv(2048)).decode()
+           f = open(f_name,"w+")
+           f.write(info)
+           print('done')
+           f.close()
     current_working_directory = os.getcwd()
     os.chdir(current_working_directory)
     files =[]
     d1=[]
     files = os.listdir(current_working_directory)
-    f_name = sock.recv(2048)        
-    if f_name in files:
-        i=files.index(f_name)
-        data = current_working_directory+f_name+' Size '+str(os.path.getsize(files[i]))+' Bytes  Last modified ' +str(os.path.getctime(files[i]))      
-    else:
-        data = 'File does not exist'
-    data1 = data.encode()
-    sock.send(data1)
+    data=pickle.dumps(files)
     
+    if inp == '2':
+           sock.send(data)
+           f_name = sock.recv(2048)
+           
+           if f_name in files:
+               i=files.index(f_name)
+               data = current_working_directory+f_name+' Size '+str(os.path.getsize(files[i]))+' Bytes  Last modified ' +str(os.path.getctime(files[i]))      
+           else:
+               data = 'File does not exist'
+           data = data.encode()
+           
+    else:
+           data = 'program terminated'
+    sock.send(data)
+     
 
 
 def Main():
@@ -49,16 +61,8 @@ def Main():
     while(True):
         c,addr = s.accept()
         print('client connected ip:'+str(addr))
-        t= threading.Thread(target= get_list('get_list',c))
-        t.start()
+        Thread = client_thread(addr,c)
+        Thread.start()
 if __name__ == '__main__':
     Main()
-
-
-        
-
-    
-
-
-
-    
+ 
