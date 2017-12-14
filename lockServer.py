3ed667c6be76ca6d43204385e31e89a09960c304
VikasDanny <vikasdanny56@gmail.com>
2017-12-14 14:57:11 +0000

    Update lockServer.py

--- lockServer.py
+++ lockServer.py
@@ -55,7 +55,7 @@ def lock(name,c):
     
 def Main():
     host = '127.0.0.1'
-    port = 5003
+    port = 6002
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(('',port))
     s.listen(5)
